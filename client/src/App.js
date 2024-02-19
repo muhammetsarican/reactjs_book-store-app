238f8755fbb97b31eea0eef24c9efb71aba75fcb
muhammetsarican <muhammetsarican@gmail.com>
2024-02-20 00:39:51 +0300

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import BookList from "./components/BookList";
+
+function App() {
+  return (
+    <div className="App flex flex-col justify-center items-center bg-[#eee] min-h-dvh ">
+      <h1 className='font-nunito font-bold text-5xl'>Graphql React App</h1>
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
